81060ad97921ab66886d83300158e618b3474b17
kumanuron-1910 <kumanuron1910.cyc5@gmail.com>
2022-12-28 18:16:38 +0900

    first commit

---
+++ ipscan.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+TEXTFILE="ipscan_result.txt"
+
+# テキストファイルの初期化
+if [ -e $TEXTFILE ]
+then
+	cp /dev/null $TEXTFILE
+fi
+
+echo "Scanning for IP address..."
+
+# ネットワーク内すべてのIPアドレスの疎通確認
+count=0
+for x in {1..254}
+do
+	if ping -c 4 192.168.1.$x | grep -i "ttl" >& /dev/null
+	then
+		echo "[*] Connected : 192.168.1.$x"
+		echo 192.168.1.$x >> $TEXTFILE
+		count=`expr $count + 1`
+	fi
+done
+
+echo "Scanning is completed!!"
+echo "The text file has been created."
+printf "\n"
+echo "------------ Result ------------"
+echo "Total IP address connected : $count"
+echo "Created text file : $TEXTFILE"
+printf "\n"
